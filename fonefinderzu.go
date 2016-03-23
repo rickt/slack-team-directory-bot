daa0eb67b7eed27a775b357a6b8d07edd387aec9
rick tait <rickt@rickt.org>
2016-03-23 15:40:43 -0700

    * adding app_default.yaml * adding types.go * adding fonefinderzu.go

---
+++ fonefinderzu.go
@@ -0,0 +1,136 @@
+package fonefinderzu
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gorilla/schema"
+	"github.com/rickt/slack-appengine"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"     // required to use the appengine logging package
+	"google.golang.org/appengine/log" // appengine logging package
+	"net/http"
+	"os"
+	"strings"
+)
+
+// vars
+var (
+	env envs
+)
+
+// helper function to do a case-insensitive search
+func ciContains(a, b string) bool {
+	return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
+}
+
+// no main() since this is an appengine app
+func init() {
+	// setup the url handler
+	http.HandleFunc("/slack", slackhandler)
+}
+
+// func that handles the POST to /slack from slack
+func slackhandler(w http.ResponseWriter, r *http.Request) {
+	// get runtime options from the app.yaml
+	// w/appengine, we can't query environment variables until after init() completes. sucks.jpg
+	isdebug := os.Getenv("DEBUG")
+	if isdebug == "true" {
+		env.Debug = true
+	}
+	env.Token = os.Getenv("SLACK_TOKEN")
+	env.TriggerWord = os.Getenv("SLACK_TRIGGERWORD")
+	env.UserToken = os.Getenv("SLACK_USER_TOKEN")
+	// create a google appengine context
+	ctx := appengine.NewContext(r)
+	hook := slackRequest{}
+	// get the data from the POST from slack
+	err := r.ParseForm()
+	if err != nil {
+		log.Errorf(ctx, "ERROR slackhandler(): parsing form error! err=%s", err)
+		http.NotFound(w, r)
+		return
+	}
+	defer r.Body.Close()
+	log.Infof(ctx, "DEBUG slackhandler(): env.Token=%s, env.TriggerWord=%s, env.UserToken=%s, env.Debug=%v", env.Token, env.TriggerWord, env.UserToken, env.Debug)
+	// decode slack request
+	err = decodeslackrequest(r, &hook)
+	if err != nil {
+		log.Errorf(ctx, "ERROR slackhandler(): error decoding request from slack!! err=%v", err)
+	}
+	if env.Debug {
+		log.Debugf(ctx, "DEBUG slackhandler(): hook.Text=%v, hook.Token=%s, hook.TriggerWord=%v", hook.Text, hook.Token, hook.TriggerWord)
+	}
+	// search for the user
+	userlist, err := searchforusers(hook.Text, ctx)
+	if env.Debug {
+		log.Debugf(ctx, "DEBUG slackhandler(): userlist=%v", userlist)
+	}
+	// build the response
+	var phone, response string
+	for _, user := range userlist {
+		if user.Profile.Phone == "" {
+			phone = "UNLISTED"
+		} else {
+			phone = user.Profile.Phone
+		}
+		response = response + fmt.Sprintf("%s %s: :phone: %s :email: <mailto:%s|%s> :slack: <@%s|%s>\n", user.Profile.FirstName, user.Profile.LastName, phone, user.Profile.Email, user.Profile.Email, user.Id, user.Name)
+	}
+	payload := Payload{
+		// ResponseType: "in_channel",
+		Text: response,
+	}
+	if env.Debug {
+		log.Debugf(ctx, "DEBUG slackhandler(): payload=%v", payload)
+	}
+	// json it up & send it
+	js, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+// func that decodes the slack request
+func decodeslackrequest(r *http.Request, hook *slackRequest) error {
+	// create a google appengine context
+	ctx := appengine.NewContext(r)
+	// create a decoder
+	decoder := schema.NewDecoder()
+	// decode
+	err := decoder.Decode(hook, r.PostForm)
+	if err != nil {
+		log.Warningf(ctx, "DEBUG decodeslackrequest(): decode error! err=%s, hook=%v", err, hook)
+	}
+	// is this a valid request from slack? check for the token
+	if env.Token != hook.Token {
+		log.Errorf(ctx, "ERROR decodeslackrequest(): token received from slack hook.Token=%s does not match expected token env.Token=%s!", hook.Token, env.Token)
+		// return errors.New("error, token received from slack does not match expected token!")
+	}
+	return nil
+}
+
+// search for the user
+func searchforusers(name string, ctx context.Context) ([]*slack.User, error) {
+	if env.UserToken == "" {
+		return nil, errors.New("error, slack user token is invalid!")
+	}
+	// establish slack connection
+	sl := slack.New(env.UserToken, ctx)
+	users, err := sl.UsersList()
+	if err != nil {
+		log.Errorf(ctx, "error retrieving userlist, err=%s", err)
+		return nil, err
+	}
+	// find the name we were given
+	var searchedusers []*slack.User
+	for _, user := range users {
+		if strings.Contains(user.Profile.Email, "@") {
+			if ciContains(user.Name, name) || ciContains(user.Profile.RealName, name) || ciContains(user.Profile.FirstName, name) || ciContains(user.Profile.LastName, name) {
+				searchedusers = append(searchedusers, user)
+				log.Debugf(ctx, "DEBUG   Id=%s, Name=%s, RealName=%s, Phone=%s", user.Id, user.Name, user.Profile.RealName, user.Profile.Phone)
+			}
+		}
+	}
+	return searchedusers, nil
+}
+
+// EOF
---
+++ types.go
@@ -0,0 +1,55 @@
+package fonefinderzu
+
+// struct for runtime environment variables
+type envs struct {
+	Debug       bool
+	Token       string
+	TriggerWord string
+	UserToken   string
+}
+
+// struct for forming a slack request
+type slackRequest struct {
+	Token       string `schema:"token"`
+	TeamID      string `schema:"team_id"`
+	TeamDomain  string `schema:"team_domain"`
+	ChannelID   string `schema:"channel_id"`
+	ServiceID   string `schema:"service_id"`
+	ChannelName string `schema:"channel_name"`
+	Timestamp   string `schema:"timestamp"`
+	UserID      string `schema:"user_id"`
+	UserName    string `schema:"user_name"`
+	Text        string `schema:"text"`
+	TriggerWord string `schema:"trigger_word"`
+}
+
+// structs for slack inbound webhook message
+type Payload struct {
+	Channel      string       `json:"channel"`
+	Username     string       `json:"username"`
+	Text         string       `json:"text"`
+	ResponseType string       `json:"response_type"`
+	Icon_emoji   string       `json:"icon_emoji"`
+	Unfurl_links bool         `json:"unfurl_links"`
+	Attachments  []Attachment `json:"attachments"`
+}
+type Attachment struct {
+	Fallback   string  `json:"fallback"`
+	Pretext    string  `json:"pretext"`
+	Color      string  `json:"color"`
+	AuthorName string  `json:"author_name"`
+	AuthorLink string  `json:"author_link"`
+	AuthorIcon string  `json:"author_icon"`
+	Title      string  `json:"title"`
+	TitleLink  string  `json:"title_link"`
+	Text       string  `json:"text"`
+	Fields     []Field `json:"fields"`
+}
+
+type Field struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short"`
+}
+
+// EOF
